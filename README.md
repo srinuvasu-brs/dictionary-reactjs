e6f331b8039c80c727e1071cb623e3d73e547fb9
srinuvasu-brs <srinumsc@gmail.com>
2024-06-18 11:31:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+ # Dictionary App
+
+##### API:
+    Dictionary API
+
+##### API link:
+    https://dictionaryapi.dev/
+
+##### Purpose :
+    Get definitions of a word using API
+
+#### Dear Students, Check out the live app [here](http://203.193.173.125/buildriseshine/api/reactjs/dictionary/)
---
+++ src/core/store/app-context.js
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react';
+import { HttpGet } from './httpHelper';
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const contextValue = { HttpGet }
+    return (
+      <AppContext.Provider value={contextValue}>
+        {children}
+      </AppContext.Provider>
+    )
+}
